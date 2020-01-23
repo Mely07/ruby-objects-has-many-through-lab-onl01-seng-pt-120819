8e17af8c33e02bad864eb61defd61df61c5dd1ec
Damely Tineo <damely.tineo07@gmail.com>
2020-01-23 23:59:49 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+# Joining model between Artist & Genre Classes
+
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@all = []
+ 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    save
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def save 
+    @@all << self
+  end
+  
+end
